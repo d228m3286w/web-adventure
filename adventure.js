17c3743e943943486fc4023f09d0b8bcc31c16b1
d228m3286w <d228m3286w@gmail.com>
2015-10-01 16:42:15 -0600

    ape with key movment

--- adventure.js
+++ adventure.js
@@ -2,65 +2,93 @@
 
 // Initializing the corrdinates of the target cell 
 
-console.log('hello');
 
-var adventure =  {
-	yCoord: 0,
-	xCoord: 0,
-	endX: 8,
-	endY: 8,
 
-	mov: function(xDel, yDel, x, y, bound) {
 
-		if(this.xCoord === (this.endX + xDel) && this.yCoord === (this.endY + yDel)) {
-        	this.xCoord += x;
-        	this.yCoord += y;
-        	hideAbe(xDel, yDel);	
-        	killAbe();
-       		dead.play();
-       		alert("Whuuthaa!!??");
-		} else if (this.yCoord === bound && yDel != 0) {
-            holdon.play();
-            alert("D'oh!!!");
-    	} else if (this.xCoord === bound && xDel != 0) {
-        	coon.play();
-        	alert("D'oh!!!");
-    	} else {
-        	this.xCoord += x;
-        	this.yCoord += y;
-			hideAbe(xDel, yDel);
-			movAbe();
-		}
+function Adventure(xCoord, yCoord, endX, endY, bound) {
+	this.xCoord = xCoord;
+	this.yCoord = yCoord;
+	this.endX = endX;
+	this.endY = endY;
+	this.bound = bound;
 
-		var movRight = function(){
-			alert("hello")
-		}
 
-		// movDown: function () {
-
-		// 	mov(0, -1, 0, 1, 8);
-		// },
-
-		// movUp: function () {
-		// 	mov(0, 1, 0, -1, 0);
-		// },
-
-		// movLeft: function () {
-		// 	mov(1, 0, -1, 0, 0);
-		// },
-
-		// movRight: function () {
-		// 	mov(-1, 0, 1, 0, 8);
-		// },
-
-		// reset: function () {
-	 //    	hideAbe(0,0);
-	 //    	this.xCoord = 0;
-	 //    	this.yCoord = 0;ß
-	 //    	movAbe();
-	 //    	target();
-		// }
-	}
 
 
 };
+var newAdventure = new Adventure(0, 0, 8, 8, 8);
+
+newAdventure.mov = function(xDel, yDel, x, y, bound) {
+	console.log(newAdventure);
+	if(this.xCoord === (this.endX + xDel) && this.yCoord === (this.endY + yDel)) { 
+        this.xCoord += x;
+        this.yCoord += y;
+        hideAbe(xDel, yDel);	
+        killAbe();
+        dead.play();
+        alert("Whuuthaa!!??");
+	}
+	else if (this.yCoord === bound && yDel != 0) {
+        holdon.play();
+        alert("D'oh!!!");
+    }
+    else if (this.xCoord === bound && xDel != 0) {
+        coon.play();
+        alert("D'oh!!!");
+    }
+        else {
+        this.xCoord += x;
+        this.yCoord += y;
+		hideAbe(xDel, yDel);
+		movAbe();
+	};
+}
+
+newAdventure.movDown = function() {
+	this.mov(0, -1, 0, 1, 8);
+}; 
+newAdventure.movUp = function() {
+	this.mov(0, 1, 0, -1, 0);
+}; 
+newAdventure.movLeft = function() {
+	this.mov(1, 0, -1, 0, 0);
+}; 
+newAdventure.movRight = function() {
+	this.mov(-1, 0, 1, 0, 8);
+};
+
+newAdventure.reset = function() {
+
+    hideAbe(0,0);
+    this.xCoord = 0;
+    this.yCoord = 0;
+    movAbe();
+    target();
+    console.log('hello');
+};
+
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+            newAdventure.movLeft();
+            break;
+        case 38:
+            newAdventure.movUp();
+            break;
+        case 39:
+            newAdventure.movRight();
+            break;
+        case 40:
+            newAdventure.movDown();
+			break;
+		case 32:
+			newAdventure.reset();
+
+    }
+};
+
+
+
+
+
+
--- app.js
+++ app.js
@@ -2,22 +2,22 @@
 
 //Adding a function to replace the winning cell with a picture of dead Abe Simpson
 var killAbe = function () {
-	document.getElementById(adventure.endY.toString() + adventure.endX.toString()).innerHTML = "<img src=\"dead.jpg\"></img>";
+	document.getElementById(newAdventure.endY.toString() + newAdventure.endX.toString()).innerHTML = "<img src=\"dead.jpg\"></img>";
 };
 
 //Adding a function to place an image at the target cell
 var target = function(){
-	document.getElementById(adventure.endY.toString() + adventure.endX.toString()).innerHTML = "<img src=\"retire.png\"></img>";
+	document.getElementById(newAdventure.endY.toString() + newAdventure.endX.toString()).innerHTML = "<img src=\"retire.png\"></img>";
 };
     
 var movAbe = function(){ 
-	document.getElementById(adventure.yCoord.toString() + adventure.xCoord.toString()).innerHTML = "<img src=\"small_abe.png\"></img>";
+	document.getElementById(newAdventure.yCoord.toString() + newAdventure.xCoord.toString()).innerHTML = "<img src=\"small_abe.png\"></img>";
 };
 
 //refactoring hideAbe
 
 var hideAbe = function (xChange, yChange) {
-	document.getElementById((adventure.yCoord + yChange).toString() + (adventure.xCoord + xChange).toString()).innerHTML = "";
+	document.getElementById((newAdventure.yCoord + yChange).toString() + (newAdventure.xCoord + xChange).toString()).innerHTML = "";
 }
 
 var holdon = new Audio('holdon.wav');
@@ -28,49 +28,3 @@ var dead = new Audio('dead.wav');
 movAbe();
 target();
 
-function mov(xDel, yDel, x, y, bound) {
-	
-	if(adventure.xCoord === (adventure.endX + xDel) && adventure.yCoord === (adventure.endY + yDel)) {
-        adventure.xCoord += x;
-        adventure.yCoord += y;
-        hideAbe(xDel, yDel);	
-        killAbe();
-        dead.play();
-        alert("Whuuthaa!!??");
-	}
-	else if (adventure.yCoord === bound && yDel != 0) {
-        holdon.play();
-        alert("D'oh!!!");
-    }
-    else if (adventure.xCoord === bound && xDel != 0) {
-        coon.play();
-        alert("D'oh!!!");
-    }
-        else {
-        adventure.xCoord += x;
-        adventure.yCoord += y;
-		hideAbe(xDel, yDel);
-		movAbe();
-	};
-}
-
-function movDown() {
-	mov(0, -1, 0, 1, 8);
-}; 
-function movUp() {
-	mov(0, 1, 0, -1, 0);
-}; 
-function movLeft() {
-	mov(1, 0, -1, 0, 0);
-}; 
-function movRight() {
-	mov(-1, 0, 1, 0, 8);
-};
-
-function reset() {
-    hideAbe(0,0);
-    adventure.xCoord = 0;
-    adventure.yCoord = 0;
-    movAbe();
-    target();
-}
